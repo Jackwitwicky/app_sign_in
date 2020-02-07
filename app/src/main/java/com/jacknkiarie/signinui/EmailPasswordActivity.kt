c9b6d4622d03865f9357c655cc49df1bed893031
Jack Kiarie <jacknkiarie@gmail.com>
2020-02-07 14:38:45 +0300

    removed the splash screen and updated name of email password screen

--- app/src/main/java/com/jacknkiarie/signinui/EmailPasswordActivity.kt
+++ app/src/main/java/com/jacknkiarie/signinui/EmailPasswordActivity.kt
@@ -3,16 +3,16 @@ package com.jacknkiarie.signinui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_email_password.*
 
-class MainActivity : AppCompatActivity() {
+class EmailPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_email_password)
 
         email_password_pin_login.setOnClickListener {
-            val pinLoginIntent = Intent(this@MainActivity, PinActivity::class.java)
+            val pinLoginIntent = Intent(this@EmailPasswordActivity, PinActivity::class.java)
             startActivity(pinLoginIntent)
         }
     }
