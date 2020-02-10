8dda0178452f2a2ca4a3b1315fbce278bdc63681
Jackwitwicky <jacknkiarie@gmail.com>
2020-02-10 18:07:29 +0300

    modified fingerprint screen to update based on builder parameters

--- app/src/main/java/com/jacknkiarie/signinui/FingerprintActivity.kt
+++ app/src/main/java/com/jacknkiarie/signinui/FingerprintActivity.kt
@@ -2,6 +2,7 @@ package com.jacknkiarie.signinui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.jacknkiarie.signinui.models.SignInUI
 import kotlinx.android.synthetic.main.activity_fingerprint.*
@@ -12,7 +13,7 @@ class FingerprintActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fingerprint)
 
-        var fingerprintWrapper = FingerprintWrapper(this)
+        val fingerprintWrapper = FingerprintWrapper(this)
         fingerprintWrapper.checkFingerprintSupportAndAuthenticate()
 
         fingerprint_prompt_button.setOnClickListener {
@@ -39,5 +40,29 @@ class FingerprintActivity : AppCompatActivity() {
             finish()
         }
 
+        setupUI()
+    }
+
+    private fun setupUI() {
+        val isEmailEnabled = intent.getBooleanExtra(SignInUI.EXTRA_IS_EMAIL_ENABLED, false)
+        val isPinEnabled = intent.getBooleanExtra(SignInUI.EXTRA_IS_PIN_ENABLED, false)
+        val title = intent.getStringExtra(SignInUI.EXTRA_TITLE)
+        val subtitle = intent.getStringExtra(SignInUI.EXTRA_SUBTITLE)
+
+        if(title != null && title.isNotEmpty()) {
+            fingerprint_welcome.text = title
+        }
+
+        if(subtitle != null && subtitle.isNotEmpty()) {
+            fingerprint_intro.text = subtitle
+        }
+
+        if (!isPinEnabled) {
+            fingerprint_pin_login.visibility = View.GONE
+        }
+
+        if (!isEmailEnabled) {
+            fingerprint_password_login.visibility = View.GONE
+        }
     }
 }
