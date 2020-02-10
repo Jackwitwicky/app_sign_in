43c8ca4af0c6fb8828df243859d77b52206a4a5f
katungi <dankatdennis@gmail.com>
2020-02-10 08:42:10 +0300

    all buttons are working fine

--- app/src/main/java/com/jacknkiarie/signinui/PinActivity.kt
+++ app/src/main/java/com/jacknkiarie/signinui/PinActivity.kt
@@ -15,9 +15,57 @@ class PinActivity : AppCompatActivity() {
     }
 
     fun onClick(view: View) {
-        if(view.id == R.id.pin_one) {
+        if(view.id == R.id.pin_one ) {
             currentPinInput += "1"
             pin_verification_code.setText(currentPinInput)
         }
+        else if(view.id == R.id.pin_two){
+            currentPinInput += "2"
+            pin_verification_code.setText(currentPinInput)
+        }
+        else if(view.id == R.id.pin_three){
+            currentPinInput += "3"
+            pin_verification_code.setText(currentPinInput)
+        }
+        else if(view.id == R.id.pin_four){
+            currentPinInput += "4"
+            pin_verification_code.setText(currentPinInput)
+        }
+        else if(view.id == R.id.pin_five){
+            currentPinInput += "5"
+            pin_verification_code.setText(currentPinInput)
+        }
+        else if(view.id == R.id.pin_six){
+            currentPinInput += "6"
+            pin_verification_code.setText(currentPinInput)
+        }
+        else if(view.id == R.id.pin_seven){
+            currentPinInput += "7"
+            pin_verification_code.setText(currentPinInput)
+        }
+        else if(view.id == R.id.pin_eight){
+            currentPinInput += "8"
+            pin_verification_code.setText(currentPinInput)
+        }
+        else if(view.id == R.id.pin_nine) {
+            currentPinInput += "9"
+            pin_verification_code.setText(currentPinInput)
+        }
+
+        if (view.id == R.id.pin_check_btn){
+
+        }
+
     }
+
+
+
+
+
+
+
+
+
+
+
 }
